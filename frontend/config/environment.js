5314fbbafee332d736bc190890b9362084192616
Arthur Deierlein <arthur.deierlein@adfinis.com>
2024-05-24 13:15:50 +0200

    fix(frontend): upgrade to ember v4.12

--- frontend/config/environment.js
+++ frontend/config/environment.js
@@ -12,6 +12,7 @@ module.exports = function (environment) {
       includeTimezone: "all",
     },
     EmberENV: {
+      EXTEND_PROTOTYPES: false,
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
