cf8384a199afc7b75c7630f96843e71e0e955d8c
Arthur Deierlein <arthur.deierlein@adfinis.com>
2024-07-10 08:43:06 +0200

    chore(frontend): fix typos in parseFileName util tests

--- frontend/tests/unit/utils/parse-filename-test.js
+++ frontend/tests/unit/utils/parse-filename-test.js
@@ -4,25 +4,25 @@ import parseFileName from "timed/utils/parse-filename";
 module("Unit | Helper | parse filename", function () {
   test("works with double quotes", function (assert) {
     const result = parseFileName(
-      'attachement; filename="2407-20240708-Customer-Big_Project.ods"'
+      'attachment; filename="1805-20240710-Customer-Sample_Project.ods"'
     );
 
-    assert.strictEqual(result, "2407-20240708-Customer-Big_Project.ods");
+    assert.strictEqual(result, "1805-20240710-Customer-Sample_Project.ods");
   });
 
   test("works with single quotes", function (assert) {
     const result = parseFileName(
-      "attachement; filename='2407-20240708-Customer-Big_Project.ods'"
+      "attachment; filename='1805-20240710-Customer-Sample_Project.ods'"
     );
 
-    assert.strictEqual(result, "2407-20240708-Customer-Big_Project.ods");
+    assert.strictEqual(result, "1805-20240710-Customer-Sample_Project.ods");
   });
 
   test("works without quotes", function (assert) {
     const result = parseFileName(
-      "attachement; filename=2407-20240708-Customer-Big_Project.ods"
+      'attachment; filename=1805-20240710-Customer-Sample_Project.ods'
     );
 
-    assert.strictEqual(result, "2407-20240708-Customer-Big_Project.ods");
+    assert.strictEqual(result, "1805-20240710-Customer-Sample_Project.ods");
   });
 });
