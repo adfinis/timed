ca8f26e86626647e7d3c793c6186a722a724d8e1
Arthur Deierlein <arthur.deierlein@adfinis.com>
2024-09-25 16:02:00 +0200

    chore(frontend/tailwind): add branding colors

--- frontend/config/tailwind/tailwind.config.js
+++ frontend/config/tailwind/tailwind.config.js
@@ -1,16 +1,34 @@
-'use strict';
+"use strict";
 // located in <app root>/config/tailwind/
 
-const path = require('path');
+const path = require("path");
 
-const appRoot = path.join(__dirname, '../../');
-const appEntry = path.join(appRoot, 'app');
-const relevantFilesGlob = '**/*.{html,js,ts,hbs,gjs,gts}';
+const appRoot = path.join(__dirname, "../../");
+const appEntry = path.join(appRoot, "app");
+const relevantFilesGlob = "**/*.{html,js,ts,hbs,gjs,gts}";
 
 module.exports = {
   content: [path.join(appEntry, relevantFilesGlob)],
   theme: {
-    extend: {},
+    theme: {
+      fontFamily: {
+        sans: ["Source Sans Pro"],
+      },
+      extend: {
+        colors: {
+          adfinis: {
+            blue: "#2e4b98",
+            darkblue: "#1c2e5d",
+            green: "#2e987b",
+            grey: "#f5f6f5",
+            darkgrey: "#8b8b8c",
+            black: "#0f0f0f",
+            white: "#f8f7f7",
+          },
+          black: "#0f0f0f",
+          white: "#f8f7f7",
+        },
+      },
+    },
   },
-  plugins: [],
 };
