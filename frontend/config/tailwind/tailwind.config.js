1aaf644e7b04970d73c46690ae372fd40310b1a6
Arthur Deierlein <arthur.deierlein@adfinis.com>
2024-09-26 15:03:48 +0200

    progressc

--- frontend/config/tailwind/tailwind.config.js
+++ frontend/config/tailwind/tailwind.config.js
@@ -3,25 +3,31 @@
 
 const path = require("path");
 
+const forms = require("@tailwindcss/forms");
+
 const appRoot = path.join(__dirname, "../../");
 const appEntry = path.join(appRoot, "app");
 const relevantFilesGlob = "**/*.{html,js,ts,hbs,gjs,gts}";
 
 module.exports = {
   content: [path.join(appEntry, relevantFilesGlob)],
+  safelist: [{ pattern: /ember-/ }],
+  plugins: [forms()],
   theme: {
-    colors: {
-      adfinis: {
-        blue: "#2e4b98",
-        darkblue: "#1c2e5d",
-        green: "#2e987b",
-        grey: "#f5f6f5",
-        darkgrey: "#8b8b8c",
+    extend: {
+      colors: {
+        adfinis: {
+          blue: "#2e4b98",
+          darkblue: "#1c2e5d",
+          green: "#2e987b",
+          grey: "#f5f6f5",
+          darkgrey: "#8b8b8c",
+          black: "#0f0f0f",
+          white: "#f8f7f7",
+        },
         black: "#0f0f0f",
         white: "#f8f7f7",
       },
-      black: "#0f0f0f",
-      white: "#f8f7f7",
     },
     fontFamily: {
       sans: ["Source Sans Pro"],
