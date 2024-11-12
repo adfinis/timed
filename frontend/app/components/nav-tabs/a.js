5665714d8232bf5c345a786df7545dedbc9a664a
Arthur Deierlein <arthur.deierlein@adfinis.com>
2024-11-12 15:45:42 +0100

    fix(frontend): don't show small absence and add absence buttons when big ones are shown

--- frontend/app/components/nav-tabs/a.js
+++ frontend/app/components/nav-tabs/a.js
@@ -2,7 +2,7 @@ import Component from "@glimmer/component";
 
 export default class NavTabsAComponent extends Component {
   get class() {
-    return "group max-sm:flex max-sm:justify-center sm:flex sm:items-center lg:text-lg transition-[font-size] gap-x-2 py-2 px-4 sm:border-background sm:border sm:bg-background sm:-mb-px sm:border-b-border group";
+    return "group max-sm:flex max-sm:justify-center sm:flex sm:items-center lg:text-lg transition-[font-size] gap-x-1 md:gap-x-2 py-2 px-4 sm:border-background sm:border sm:bg-background sm:-mb-px sm:border-b-border group text-sm md:text-base";
   }
 
   get activeClass() {
