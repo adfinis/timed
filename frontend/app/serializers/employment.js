adccf3fb8a6a1eab0526be5644f9141cef7bf005
Arthur Deierlein <arthur.deierlein@adfinis.com>
2024-12-09 15:55:50 +0100

    refactor(frontend): use javascripts extends instead of Thing.extend

--- frontend/app/serializers/employment.js
+++ frontend/app/serializers/employment.js
@@ -12,20 +12,15 @@ import ApplicationSerializer from "timed/serializers/application";
  * @extends ApplicationSerializer
  * @public
  */
-export default ApplicationSerializer.extend({
+export default class EmploymentSerializer extends ApplicationSerializer {
   /**
    * The attribute mapping
    *
    * This mapps some properties of the response to another
    * property name of the model
-   *
-   * @property {Object} attrs
-   * @property {String} start
-   * @property {String} end
-   * @public
    */
-  attrs: {
+  attrs = {
     start: "start-date",
     end: "end-date",
-  },
-});
+  };
+}
