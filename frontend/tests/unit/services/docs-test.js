18c62072f938c9ec4f3fd8b83f34693f7655f54e
Mitan Omar <mitan@gmx.ch>
2025-01-20 10:42:11 +0100

    feat: add timed docs to top nav

---
+++ frontend/tests/unit/services/docs-test.js
@@ -0,0 +1,12 @@
+import { module, test } from "qunit";
+import { setupTest } from "timed/tests/helpers";
+
+module("Unit | Service | docs", function (hooks) {
+  setupTest(hooks);
+
+  // TODO: Replace this with your real tests.
+  test("it exists", function (assert) {
+    const service = this.owner.lookup("service:docs");
+    assert.ok(service);
+  });
+});
