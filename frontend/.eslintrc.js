413cf12d239159e0145ab387a0b865c999f99850
Arthur Deierlein <arthur.deierlein@adfinis.com>
2025-02-17 13:17:30 +0100

    chore(frontend/eslint): make ember/no-get error instead of warn

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,7 +3,6 @@
 module.exports = {
   extends: ["@adfinis/eslint-config/ember-app"],
   rules: {
-    "ember/no-get": "warn",
     "ember/no-observers": "warn",
     "qunit/no-assert-equal": "warn",
     "ember/require-tagless-components": "warn",
