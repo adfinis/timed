07d71ebce2aca4ceb7a3a5aa8b801c07519500e0
Arthur Deierlein <arthur.deierlein@adfinis.com>
2025-02-17 13:16:45 +0100

    chore(frontend/eslint): make ember/no-component-lifecycle-hooks error instead of warn

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,7 +3,6 @@
 module.exports = {
   extends: ["@adfinis/eslint-config/ember-app"],
   rules: {
-    "ember/no-component-lifecycle-hooks": "warn",
     "ember/no-get": "warn",
     "ember/no-observers": "warn",
     "qunit/no-assert-equal": "warn",
