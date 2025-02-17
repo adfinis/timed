971aa76fb6a3e0816a5d6c6d99dbabd2735566a3
Arthur Deierlein <arthur.deierlein@adfinis.com>
2025-02-17 13:12:32 +0100

    chore(frontend/eslint): make ember/no-actions-hash error instead of warn

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,7 +3,6 @@
 module.exports = {
   extends: ["@adfinis/eslint-config/ember-app"],
   rules: {
-    "ember/no-actions-hash": "warn",
     "ember/no-component-lifecycle-hooks": "warn",
     "ember/no-mixins": "warn",
     "ember/no-new-mixins": "warn",
