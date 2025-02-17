b8eb222ccf2f222d1c83158bf06e27d7d0413e10
Arthur <110528300+c0rydoras@users.noreply.github.com>
2025-02-17 13:31:40 +0100

    chore(frontend): make eslint config stricter (#609)
    * chore(frontend/eslint): make ember/no-actions-hash error instead of warn
    
    * chore(frontend/eslint): make ember/no-*mixins error instead of warn
    
    * chore(frontend/eslint): make ember/no-classic-* error instead of warn
    
    * chore(frontend/eslint): make ember/no-component-lifecycle-hooks error instead of warn
    
    * chore(frontend/eslint): make ember/no-get error instead of warn
    
    * chore(frontend/eslint): make ember/require-tagless-components error instead of warn
    
    * chore(frontend/eslint): make qunit/no-assert-equal error instead of warn

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,15 +3,6 @@
 module.exports = {
   extends: ["@adfinis/eslint-config/ember-app"],
   rules: {
-    "ember/no-actions-hash": "warn",
-    "ember/no-component-lifecycle-hooks": "warn",
-    "ember/no-mixins": "warn",
-    "ember/no-new-mixins": "warn",
-    "ember/no-classic-classes": "warn",
-    "ember/no-classic-components": "warn",
-    "ember/no-get": "warn",
     "ember/no-observers": "warn",
-    "qunit/no-assert-equal": "warn",
-    "ember/require-tagless-components": "warn",
   },
 };
