50085785fe7e08557ac0d1879884296459c1dc0c
Arthur Deierlein <arthur.deierlein@adfinis.com>
2025-02-18 13:45:26 +0100

    chore(frontend): update linting
    update to eslint v9
    update most eslint-* deps
    update stylelint
    etc.

--- frontend/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-"use-strict";
-
-module.exports = {
-  extends: ["@adfinis/eslint-config/ember-app"],
-  rules: {
-    "ember/no-observers": "warn",
-  },
-};
---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,16 @@
+import adfinisEmberAppConfig from "@adfinis/eslint-config/ember-app";
+import ember from "eslint-plugin-ember";
+
+export default [
+  ...adfinisEmberAppConfig,
+  {
+    plugins: { ember },
+    settings: {
+      "import/internal-regex": "^timed/",
+    },
+    rules: {
+      "ember/no-runloop": "warn",
+      "ember/no-observers": "warn",
+    },
+  },
+];
