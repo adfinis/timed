fb6ad61d17590dd30b71af627450e20d478be38b
Mitan Omar <mitan@gmx.ch>
2025-03-20 10:07:06 +0100

    refactor(docs-service): use router name instate of uri

--- frontend/app/services/docs.js
+++ frontend/app/services/docs.js
@@ -1,31 +1,26 @@
 import Service, { service } from "@ember/service";
 
+const docsUrlMatch = {
+  // index.name: DocsUrl
+  "index.attendances": "tracking/attendances",
+  "index.reports": "tracking/timesheet",
+  "analysis.index": "analysis",
+  statistics: "statistics",
+  projects: "projects",
+  "users.index": "users",
+  "users.edit.index": "users",
+  "index.activities.index": "tracking/activities",
+};
+
 export default class DocsService extends Service {
   timedDocsURL = "https://timed.dev/docs/";
   @service router;
-
-  docsUrlMatch = {
-    // index.name: DocsUrl
-    "index.attendances": "tracking/attendances",
-    "index.reports": "tracking/timesheet",
-    "analysis.index": "analysis",
-    statistics: "statistics",
-    projects: "projects",
-    "users.index": "users",
-    "users.edit.index": "users",
-    "index.activities.index": "tracking/activities",
-  };
 
   get docsEndpoint() {
     return this.timedDocsURL + this.getDocsURL;
   }
 
   get getDocsURL() {
-    for (const timedRouterName of Object.keys(this.docsUrlMatch)) {
-      if (this.router.currentRouteName === timedRouterName) {
-        return this.docsUrlMatch[timedRouterName];
-      }
-    }
-    return "";
+    return docsUrlMatch[this.router.currentRouteName] || "";
   }
 }
