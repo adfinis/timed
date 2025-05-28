72eb82c71d3c7f8d8dfeca781f616cf0fa96e140
Falk <falk.neumann@adfinis.com>
2025-05-28 17:52:12 +0200

    feat(timed-clock): dye clock blackground relative to overtime credit
    The clock will become an indicator for negative and positive overtime
    and adapts it's color accordingly. Further it will show overtime on
    hover.

--- frontend/app/components/timed-clock.js
+++ frontend/app/components/timed-clock.js
@@ -1,4 +1,5 @@
 import { setProperties } from "@ember/object";
+import { service } from "@ember/service";
 import { isTesting, macroCondition } from "@embroider/macros";
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
@@ -6,7 +7,28 @@ import { task, timeout } from "ember-concurrency";
 import { scheduleTask } from "ember-lifeline";
 import moment from "moment";
 
+const MAX_GREEN = 150;
+const MAX_RED = 150;
+const MAX_OVERTIME = 20;
+const MIN_OVERTIME = -20;
+
+const overtimeToColor = (overtime) => {
+  if (isNaN(overtime)) {
+    return "rgba(255,255,255,0)";
+  }
+  if (overtime < 0) {
+    const opacity = Math.max(0, Math.min(1, overtime / MIN_OVERTIME));
+    const redValue = Math.round(MAX_RED * opacity);
+    return `rgba(${redValue},0,0,${opacity})`;
+  }
+  const opacity = Math.max(0, Math.min(1, overtime / MAX_OVERTIME));
+  const greenValue = Math.round(MAX_GREEN * opacity);
+  return `rgba(0,${greenValue},0,${opacity})`;
+};
+
 export default class TimedClock extends Component {
+  @service currentUser;
+
   @tracked hour = 0;
   @tracked minute = 0;
   @tracked second = 0;
@@ -25,6 +47,7 @@ export default class TimedClock extends Component {
     super(...args);
 
     scheduleTask(this.timer, "actions", "perform");
+    scheduleTask(this.worktimeTimer, "actions", "perform");
   }
 
   timer = task(async () => {
@@ -39,4 +62,25 @@ export default class TimedClock extends Component {
       await timeout(1000);
     }
   });
+
+  worktimeTimer = task(async () => {
+    while (true) {
+      this.currentUser.worktimeBalance.perform();
+
+      if (macroCondition(isTesting())) {
+        return;
+      }
+
+      // eslint-disable-next-line no-await-in-loop
+      await timeout(60000);
+    }
+  });
+
+  get overtime() {
+    return this.currentUser.worktimeBalance.lastSuccessful?.value;
+  }
+
+  get overtimeColor() {
+    return overtimeToColor(this.overtime);
+  }
 }
