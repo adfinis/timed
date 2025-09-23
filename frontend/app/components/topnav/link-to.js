79cee10534c32ddac3d4539509109d2213360cfa
Arthur Deierlein <arthur.deierlein@adfinis.com>
2025-09-23 17:34:06 +0200

    fix(frontend/topnav): gap too big

--- frontend/app/components/topnav/link-to.js
+++ frontend/app/components/topnav/link-to.js
@@ -2,7 +2,7 @@ import Component from "@glimmer/component";
 
 export default class TopnavLinkToComponent extends Component {
   get class() {
-    return "gap-1 grid grid-cols-[auto,minmax(0,1fr)] h-full hover:[&:not(.active)]:bg-primary-light hover:text-foreground-primary items-center lg:gap-2 md:place-items-center md:self-center [&:not(.active,:hover)]:text-tertiary py-2 px-2.5  transition-[font-size] w-full";
+    return "gap-1 grid grid-cols-[auto,minmax(0,1fr)] h-full hover:[&:not(.active)]:bg-primary-light hover:text-foreground-primary items-center lg:gap-1.5 md:place-items-center md:self-center [&:not(.active,:hover)]:text-tertiary py-2 px-2.5  transition-[font-size] w-full";
   }
 
   get activeClass() {
