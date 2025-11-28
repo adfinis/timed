0e5b9339a1069ec536777b32e3570718a63f9394
Arthur Deierlein <arthur.deierlein@adfinis.com>
2025-11-28 13:53:17 +0100

    feat(ui-core/topnav): card

--- ui-core/tests/dummy/app/router.ts
+++ ui-core/tests/dummy/app/router.ts
@@ -8,5 +8,6 @@ export default class Router extends EmberRouter {
 Router.map(function () {
   this.route("table");
   this.route("button");
+  this.route("card");
   // Add route declarations here
 });
