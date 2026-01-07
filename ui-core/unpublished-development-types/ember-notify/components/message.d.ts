7a5ad1b3f148e9d869510a30649a0e469543502e
Arthur Deierlein <arthur.deierlein@adfinis.com>
2026-01-07 10:03:07 +0100

    chore(ui-core): improve/fix typing

---
+++ ui-core/unpublished-development-types/ember-notify/components/message.d.ts
@@ -0,0 +1,6 @@
+declare module "ember-notify/components/ember-notify/message" {
+  import Component from "@glimmer/component";
+
+  declare class NotifyMessage extends Component {}
+  export default NotifyMessage;
+}
