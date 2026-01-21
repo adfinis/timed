86141bf439f610708868548a952df691aab2665c
Mitan Omar <mitan@gmx.ch>
2026-01-21 15:40:26 +0100

    fix: accept empty comments when  editing reports

--- frontend/app/validations/intersection.js
+++ frontend/app/validations/intersection.js
@@ -1,7 +1,5 @@
 import validateIntersectionTask from "timed/validators/intersection-task";
-import validateNullOrNotBlank from "timed/validators/null-or-not-blank";
 
 export default {
   task: validateIntersectionTask(),
-  comment: validateNullOrNotBlank(),
 };
