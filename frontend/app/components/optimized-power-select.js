7efe8bb17e3050c8830136b458afd030cbe5959c
Mitan Omar <mitan@gmx.ch>
2026-01-21 17:53:41 +0100

    feat: wraparound in power selects

--- frontend/app/components/optimized-power-select.js
+++ frontend/app/components/optimized-power-select.js
@@ -1,9 +1,17 @@
 import { action } from "@ember/object";
 import Component from "@glimmer/component";
+import { task, timeout } from "ember-concurrency";
 
 export default class OptimizedPowerSelectComponent extends Component {
   get extra() {
     return this.args.extra ?? {};
+  }
+
+  @task
+  *highlightTask(select, item) {
+    yield timeout(0);
+    select.actions.highlight(item);
+    select.actions.scrollTo(item);
   }
 
   @action
@@ -21,6 +29,21 @@ export default class OptimizedPowerSelectComponent extends Component {
       select.actions.choose(select.highlighted, e);
       e.stopImmediatePropagation();
       return false;
+    } else if (e.keyCode === 40 && select.isOpen) {
+      const highlighted = select.results.indexOf(select.highlighted);
+      if (highlighted === select.results.length - 1) {
+        e.preventDefault();
+        this.highlightTask.perform(select, select.results[0]);
+      }
+    } else if (e.keyCode === 38 && select.isOpen) {
+      const highlighted = select.results.indexOf(select.highlighted);
+      if (highlighted === 0 || highlighted === -1) {
+        e.preventDefault();
+        this.highlightTask.perform(
+          select,
+          select.results[select.results.length - 1],
+        );
+      }
     }
   }
 }
