27b489c42685fc1a04805cc075f236067b18ed92
Arthur Deierlein <arthur.deierlein@adfinis.com>
2026-01-23 14:47:56 +0100

    chore(frontend/power-select): clean up old code

--- frontend/app/components/optimized-power-select.js
+++ frontend/app/components/optimized-power-select.js
@@ -15,15 +15,16 @@ export default class OptimizedPowerSelectComponent extends Component {
 
   @action
   onKeydown(select, e) {
+    if (!select.isOpen) {
+      return;
+    }
+
     // this implementation is heavily inspired by the enter key handling of EPS
     // https://github.com/cibernox/ember-power-select/blob/6e3d5781a105515b915d407d571698c57290f674/addon/components/power-select.ts#L519
-    if (e.keyCode === 9 && select.isOpen && select.highlighted !== undefined) {
+    if (e.key === "Tab" && select.highlighted !== undefined) {
       select.actions.choose(select.highlighted, e);
       e.stopImmediatePropagation();
       return false;
-    }
-    if (!select.isOpen) {
-      return;
     }
 
     if (!["ArrowUp", "ArrowDown"].includes(e.key)) {
