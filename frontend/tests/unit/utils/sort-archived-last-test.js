53db9094d0362555d75f3b0a6c636df52f56784d
Arthur <110528300+c0rydoras@users.noreply.github.com>
2026-03-13 16:23:50 +0100

    chore(frontend): improve tests for sort-archived-last (#1113)
    - use "correct" terminology
    - test that the original sorting logic is preserved
    - add test case for custom getters

--- frontend/tests/unit/utils/sort-archived-last-test.js
+++ frontend/tests/unit/utils/sort-archived-last-test.js
@@ -2,20 +2,43 @@ import { module, test } from "qunit";
 
 import sortArchivedLast from "timed/utils/sort-archived-last";
 
-const obj = (archived) => ({
+const obj = (o) => ({
   id: crypto.randomUUID(),
   name: crypto.randomUUID().slice(0, 10),
-  archived,
+  ...o,
 });
+
+const isSorted = (arr, compareFn) =>
+  arr.toSorted(compareFn).every((el, idx) => el === arr[idx]);
+
+const compareFn = (a, b) => (a.name > b.name ? 1 : -1);
 
 module("Unit | Utility | sort-archived-last", function () {
   test("it works", function (assert) {
     const objs = Array(20)
       .fill(0)
-      .map((_, i) => obj(i % 2 === 0));
+      .map((_, i) => obj({ archived: i % 2 === 0 }));
 
-    const result = objs.toSorted(sortArchivedLast((o) => o.name));
-    assert.ok(result.slice(10).every((o) => o.archived));
-    assert.ok(result.slice(0, 10).every((o) => !o.archived));
+    const result = objs.toSorted(sortArchivedLast(compareFn));
+    const [archived, notArchived] = [result.slice(10), result.slice(0, 10)];
+
+    assert.ok(archived.every((o) => o.archived));
+    assert.ok(notArchived.every((o) => !o.archived));
+    assert.ok(isSorted(archived, compareFn));
+    assert.ok(isSorted(notArchived, compareFn));
+  });
+
+  test("it allows custom getters", function (assert) {
+    const objs = Array(20)
+      .fill(0)
+      .map((_, i) => obj({ foo: i % 2 === 0 }));
+
+    const result = objs.toSorted(sortArchivedLast(compareFn, (o) => o.foo));
+    const [archived, notArchived] = [result.slice(10), result.slice(0, 10)];
+
+    assert.ok(archived.every((o) => o.foo));
+    assert.ok(notArchived.every((o) => !o.foo));
+    assert.ok(isSorted(archived, compareFn));
+    assert.ok(isSorted(notArchived, compareFn));
   });
 });
