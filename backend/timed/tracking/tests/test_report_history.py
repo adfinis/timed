8ebde12b470205f71b1eebad95ebf6acdf381329
Arthur Deierlein <arthur.deierlein@adfinis.com>
2026-03-30 15:46:07 +0200

    chore(backend/tracking): test report history creation on bulk move

--- backend/timed/tracking/tests/test_report_history.py
+++ backend/timed/tracking/tests/test_report_history.py
@@ -8,8 +8,9 @@ from rest_framework import status
 if TYPE_CHECKING:
     from django.test.client import Client
 
+    from timed.projects.factories import ProjectAssigneeFactory, TaskFactory
     from timed.tracking.factories import ReportHistoryFactory
-    from timed.tracking.models import ReportHistory
+    from timed.tracking.models import Report, ReportHistory
 
 
 def test_report_detail_include_history(
@@ -39,3 +40,49 @@ def test_report_list_include_history(
     json = response.json()
 
     assert len(json["included"]) == len(histories)
+
+
+def test_report_history_created_on_bulk_move(
+    internal_employee_client: Client,
+    report: Report,
+    task_factory: TaskFactory,
+    project_assignee_factory: ProjectAssigneeFactory,
+):
+    reviewer = internal_employee_client.user
+    project_assignee_factory(
+        project=report.task.project, user=reviewer, is_reviewer=True
+    )
+    initial_task = report.task
+    other_task = task_factory(project=initial_task.project)
+
+    # sanity checks
+    assert report.history.count() == 0
+    assert other_task != initial_task
+
+    url = reverse("report-bulk")
+
+    data = {
+        "data": {
+            "type": "report-bulks",
+            "id": None,
+            "attributes": {"review_comment": (review_comment := "foo bar baz.")},
+            "relationships": {
+                # move to other task
+                "task": {"data": {"type": "tasks", "id": other_task.pk}},
+            },
+        }
+    }
+
+    params = {"editable": 1, "reviewer": reviewer.pk, "id": report.pk}
+    response = internal_employee_client.post(url, data, query_params=params)
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    assert report.history.count() == 1
+
+    history = report.history.first()
+
+    assert history.report == report
+    assert history.previous == initial_task
+    assert history.next == other_task
+    assert history.actor == reviewer
+    assert history.comment == review_comment
