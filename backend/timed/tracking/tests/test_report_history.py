6978a99c9c29693042d2e3781f0f49acc42d5bea
Arthur Deierlein <arthur.deierlein@adfinis.com>
2026-03-30 16:12:15 +0200

    chore(backend/tracking): test report history creation on bulk move (multiple)

--- backend/timed/tracking/tests/test_report_history.py
+++ backend/timed/tracking/tests/test_report_history.py
@@ -5,12 +5,16 @@ from typing import TYPE_CHECKING
 from django.urls import reverse
 from rest_framework import status
 
+from timed.tracking.models import ReportHistory
+
 if TYPE_CHECKING:
     from django.test.client import Client
+    from pytest_django import DjangoAssertNumQueries
 
+    from timed.employment.models import User
     from timed.projects.factories import ProjectAssigneeFactory, TaskFactory
-    from timed.tracking.factories import ReportHistoryFactory
-    from timed.tracking.models import Report, ReportHistory
+    from timed.projects.models import Customer, Project
+    from timed.tracking.factories import ReportFactory, ReportHistoryFactory
 
 
 def test_report_detail_include_history(
@@ -42,22 +46,28 @@ def test_report_list_include_history(
     assert len(json["included"]) == len(histories)
 
 
-def test_report_history_created_on_bulk_move(
+def test_report_history_created_on_bulk_move_multiple(
     internal_employee_client: Client,
-    report: Report,
+    report_factory: ReportFactory,
+    user: User,
+    project: Project,
+    customer_factory: Customer,
     task_factory: TaskFactory,
     project_assignee_factory: ProjectAssigneeFactory,
+    django_assert_num_queries: DjangoAssertNumQueries,
+    snapshot: int,
 ):
     reviewer = internal_employee_client.user
-    project_assignee_factory(
-        project=report.task.project, user=reviewer, is_reviewer=True
-    )
-    initial_task = report.task
-    other_task = task_factory(project=initial_task.project)
+    project_assignee_factory(project=project, user=reviewer, is_reviewer=True)
+    other_customer = customer_factory()
 
     # sanity checks
-    assert report.history.count() == 0
-    assert other_task != initial_task
+    assert ReportHistory.objects.count() == 0
+    assert project.customer != other_customer
+    assert reviewer != user
+
+    dest_task = task_factory(project__customer=project.customer)
+    reports = report_factory.create_batch(1000, task__project=project, user=user)
 
     url = reverse("report-bulk")
 
@@ -65,24 +75,28 @@ def test_report_history_created_on_bulk_move(
         "data": {
             "type": "report-bulks",
             "id": None,
-            "attributes": {"review_comment": (review_comment := "foo bar baz.")},
+            "attributes": {"review_comment": (review_comment := "bar foo baz.")},
             "relationships": {
                 # move to other task
-                "task": {"data": {"type": "tasks", "id": other_task.pk}},
+                "task": {"data": {"type": "tasks", "id": dest_task.pk}},
             },
         }
     }
 
-    params = {"editable": 1, "reviewer": reviewer.pk, "id": report.pk}
-    response = internal_employee_client.post(url, data, query_params=params)
+    params = {
+        "editable": 1,
+        "reviewer": reviewer.pk,
+        "id": ",".join([str(report.pk) for report in reports]),
+    }
+
+    # catch performance regressions
+    with django_assert_num_queries(snapshot):
+        response = internal_employee_client.post(url, data, query_params=params)
+
     assert response.status_code == status.HTTP_204_NO_CONTENT
+    assert ReportHistory.objects.count() == len(reports)
 
-    assert report.history.count() == 1
-
-    history = report.history.first()
-
-    assert history.report == report
-    assert history.previous == initial_task
-    assert history.next == other_task
-    assert history.actor == reviewer
-    assert history.comment == review_comment
+    qs = ReportHistory.objects.all()
+    comments = qs.values_list("comment", flat=True).distinct()
+    assert len(comments) == 1
+    assert comments[0] == review_comment
