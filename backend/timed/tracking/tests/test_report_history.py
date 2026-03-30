8d14f557c5f7a0dfda49fa6c8686b8c609f57b27
Arthur Deierlein <arthur.deierlein@adfinis.com>
2026-03-30 15:26:46 +0200

    chore(backend/tracking): 2nd report history

--- backend/timed/tracking/tests/test_report_history.py
+++ backend/timed/tracking/tests/test_report_history.py
@@ -8,6 +8,7 @@ from rest_framework import status
 if TYPE_CHECKING:
     from django.test.client import Client
 
+    from timed.tracking.factories import ReportHistoryFactory
     from timed.tracking.models import ReportHistory
 
 
@@ -23,3 +24,18 @@ def test_report_detail_include_history(
 
     assert history["meta"]["count"] == 1
     assert history["data"][0]["id"] == str(report_history.pk)
+
+
+def test_report_list_include_history(
+    internal_employee_client: Client,
+    report_history_factory: ReportHistoryFactory,
+):
+    histories = report_history_factory.create_batch(10)
+
+    url = reverse("report-list")
+    response = internal_employee_client.get(url, query_params={"include": "history"})
+
+    assert response.status_code == status.HTTP_200_OK
+    json = response.json()
+
+    assert len(json["included"]) == len(histories)
