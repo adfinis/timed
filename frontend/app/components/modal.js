3cc55b60ea109a2b8020ead7770dfb9ca55dc6bd
Mitan Omar <mitan@gmx.ch>
2026-05-18 12:53:32 +0200

    fix: search input of ember-power-select inside modal

--- frontend/app/components/modal.js
+++ frontend/app/components/modal.js
@@ -4,4 +4,8 @@ export default class Modal extends Component {
   get target() {
     return document.getElementById("modals");
   }
+
+  get dropdownDestination() {
+    return document.getElementById("ember-basic-dropdown-wormhole");
+  }
 }
