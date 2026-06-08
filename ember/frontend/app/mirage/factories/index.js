d7123671b9052619fab357b6529a01609949cfdc
Arthur Deierlein <arthur.deierlein@adfinis.com>
2026-06-08 11:25:38 +0200

    chore(frontend): migrate to ember-mirage

---
+++ ember/frontend/app/mirage/factories/index.js
@@ -0,0 +1,55 @@
+import absence from "./absence";
+import absenceBalance from "./absence-balance";
+import absenceCredit from "./absence-credit";
+import absenceType from "./absence-type";
+import activity from "./activity";
+import attendance from "./attendance";
+import billingType from "./billing-type";
+import costCenter from "./cost-center";
+import customer from "./customer";
+import customerStatistic from "./customer-statistic";
+import employment from "./employment";
+import location from "./location";
+import monthStatistic from "./month-statistic";
+import overtimeCredit from "./overtime-credit";
+import project from "./project";
+import projectAssignee from "./project-assignee";
+import projectStatistic from "./project-statistic";
+import publicHoliday from "./public-holiday";
+import report from "./report";
+import reportIntersection from "./report-intersection";
+import task from "./task";
+import taskStatistic from "./task-statistic";
+import user from "./user";
+import userStatistic from "./user-statistic";
+import worktimeBalance from "./worktime-balance";
+import yearStatistic from "./year-statistic";
+
+export default {
+  absence,
+  absenceBalance,
+  absenceCredit,
+  absenceType,
+  activity,
+  attendance,
+  billingType,
+  costCenter,
+  customer,
+  customerStatistic,
+  employment,
+  location,
+  monthStatistic,
+  overtimeCredit,
+  project,
+  projectAssignee,
+  projectStatistic,
+  publicHoliday,
+  report,
+  reportIntersection,
+  task,
+  taskStatistic,
+  user,
+  userStatistic,
+  worktimeBalance,
+  yearStatistic,
+};
