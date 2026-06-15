6823017b5a9ac096d1fbbbc64754dd0de88a6277
Arthur Deierlein <arthur.deierlein@adfinis.com>
2026-06-15 17:02:18 +0200

    refactor(frontend): cleanup users.edit.responsibilities

--- ember/frontend/app/users/edit/responsibilities/route.js
+++ ember/frontend/app/users/edit/responsibilities/route.js
@@ -4,12 +4,4 @@ export default class UsersEditResponsitibilitesRoute extends Route {
   model() {
     return this.modelFor("users/edit");
   }
-
-  setupController(controller, model, ...args) {
-    super.setupController(controller, model, ...args);
-
-    controller.set("user", model);
-    controller.projects.perform();
-    controller.supervisees.perform();
-  }
 }
