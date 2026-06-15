3a044b4be63b4eb18b93ddea4c4fcd2d4f3135a7
Arthur Deierlein <arthur.deierlein@adfinis.com>
2026-06-15 16:52:53 +0200

    refactor(frontend): cleanup users.edit.index

--- ember/frontend/app/users/edit/index/route.js
+++ ember/frontend/app/users/edit/index/route.js
@@ -4,12 +4,4 @@ export default class UsersEditsIndexRoute extends Route {
   model() {
     return this.modelFor("users/edit");
   }
-
-  setupController(controller, model, ...args) {
-    super.setupController(controller, model, ...args);
-
-    controller.set("user", model);
-    controller.absences.perform();
-    controller.employments.perform();
-  }
 }
