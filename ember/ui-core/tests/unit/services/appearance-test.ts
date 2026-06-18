3aa6c8292d8f62cd35ed1695086176507f473888
Mitan Omar <mitan@gmx.ch>
2026-06-18 09:38:37 +0200

    refactor(appearance-test): remove useless code

--- ember/ui-core/tests/unit/services/appearance-test.ts
+++ ember/ui-core/tests/unit/services/appearance-test.ts
@@ -11,18 +11,11 @@ module("Unit | Service | Appearance", function (hook) {
 
   test("it can change theme and scheme", function (assert) {
     const appearanceService = this.owner.lookup("service:appearance");
-    assert.ok(appearanceService);
     appearanceService.loadConfiguration();
     const defaultTheme = "old";
     assert.strictEqual(defaultTheme, appearanceService.theme);
     appearanceService.setTheme("regular");
     assert.strictEqual(appearanceService.theme, "regular");
-    const colorScheme = ["dark", "light"];
-    // since it's checking prefers color scheme in the browser, then it depend on the test machine
-    assert.strictEqual(
-      colorScheme.includes(appearanceService.colorScheme),
-      true,
-    );
     const preferedColor = appearanceService.colorScheme;
     appearanceService.toggleColorScheme();
     const expectedChangeTo = preferedColor === "light" ? "dark" : "light";
